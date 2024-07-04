2f15471d353a37be735b6484ad696eae6c6b91d9
iniad <s1f102301190@iniad.org>
2024-07-04 13:32:03 +0900

    調整2

--- todo/models.py
+++ todo/models.py
@@ -4,17 +4,12 @@ from django.utils import timezone
 
 # Create your models here.
 class Task(models.Model):
-    #タイトル
-    title = models.CharField(max_length=100)
-    #完了しているか 
-    completed = models.BooleanField(default=False)
-    #登録日
-    posted_at = models.DateTimeField(default=timezone.now)
-    #締め切り
-    due_at = models.DateTimeField(null=True, blank=True)  
+    title = models.CharField(max_length=100)  # タイトル
+    completed = models.BooleanField(default=False)  # 完了しているか
+    posted_at = models.DateTimeField(default=timezone.now)  # 登録日
+    due_at = models.DateTimeField(null=True, blank=True)  # 締め切り
 
     def is_overdue(self, dt):
         if self.due_at is None:
             return False
         return self.due_at < dt
-    
